532d95be72cd5bcff59d3008ba3b467472357ff7
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2023-02-05 23:46:39 +0100

    Remove double click action

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import type { Component } from "solid-js";
 import GameScreen from "./components/GameScreen";
 
 const App: Component = () => {
+  document.ondblclick = function (e) {
+    e.preventDefault();
+  };
+
   return (
     <div class="m-0">
       <div class="absolute h-full w-full -z-10">
