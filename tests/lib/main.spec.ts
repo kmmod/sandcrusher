26b05a05da8c47df23bb4543fb2e569cc18654c3
Kamil Modzelewski <modzelewski.kamil@gmail.com>
2023-02-08 22:15:28 +0100

    Add test with support for handling PIXIjs state

---
+++ tests/lib/main.spec.ts
@@ -0,0 +1,14 @@
+import { assert, expect, test } from "vitest";
+import { Game } from "../../src/lib/main";
+
+test("Game instance initalization", () => {
+  const game = new Game();
+
+  expect(game.app).toBeDefined();
+  expect(game.app.renderer).toBeDefined();
+  expect(game.bindDragEnd).toBeDefined();
+  expect(game.bindDragMove).toBeDefined();
+  expect(game.bindResize).toBeDefined();
+  expect(game.dragTarget).toBeNull();
+  expect(game.resizeElement).toBeNull();
+});
---
+++ vitest.setup.ts
@@ -0,0 +1,5 @@
+import { vi } from "vitest";
+import "vitest-canvas-mock";
+import "pixi.js-legacy";
+
+vi.mock("pixi.js", () => vi.importActual("pixi.js-legacy"));
